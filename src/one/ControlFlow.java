22a2801348d12b0fc58dba1ef32f2b46a8a1a227
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-14 18:07:00 +0530

    control flow

---
+++ src/one/ControlFlow.java
@@ -0,0 +1,13 @@
+package one;
+
+public class ControlFlow {
+    public static void main(String[] args) {
+        //if-else
+        int age =20;
+        if (age > 18){
+            System.out.println("Welcome to our bank!");
+        }else{
+            System.out.println("not applicable");
+        }
+    }
+}

767ecc51e84d336beb6c65a1c5d08c83e8a24405
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-14 18:12:03 +0530

    control flow

--- src/one/ControlFlow.java
+++ src/one/ControlFlow.java
@@ -23,5 +23,14 @@ public class ControlFlow {
         }else{
             System.out.println("F");
         }
+
+        //switch
+//        String day="monday";
+        String day = "Saturday";
+        switch (day) {
+            case "Monday" -> System.out.println("Start of the week");
+            case "Friday" -> System.out.println("Last day of the week");
+            default -> System.out.println("Enjoy weekend");
+        }
     }
 }
